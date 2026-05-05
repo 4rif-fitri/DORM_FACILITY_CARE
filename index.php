1c6b76b52f83875671e47cb74223d733aabd287c
Arif <dopymonster@gmail.com>
2026-05-05 21:00:14 +0800

    update

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-</head>
-<body>
-	
-</body>
-</html>
+<?php include("/component/header.php"); ?>
+
+
+<h1>
+	LOREM
+</h1>
+
+<?php include("/component/footer.php"); ?>
---
+++ user/pages/dashboard.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+
+include(BASE_PATH . "/component/header.php");
